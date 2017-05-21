9dec4ef46e7356b47af5b6619e3122b47eedb674
happyahluwalia <happyahluwalia@gmail.com>
2017-05-21 12:58:09 -0400

    Precaching options set correctly

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -10,17 +10,25 @@ module.exports = {
   stripPrefix: 'dist/',
   navigateFallback: '/index.html',
   runtimeCaching: [{
-  urlPattern: /bbc-news/,
-  handler: 'networkFirst',
-  name: 'bbc-cache',
-  maxEnteries: 10,
-  maxAgeSeconds: 300
-},
-{
-  urlPattern: /cnn/,
-  handler: 'cacheFirst',
-  name: 'cnn-cache',
-  maxEnteries: 10,
-  maxAgeSeconds: 300
-}]
+    urlPattern: /bbc-news/,
+    handler: 'networkFirst',
+    options: {
+      cache:{
+      name: 'bbc-cache',
+      maxEnteries: 10,
+      maxAgeSeconds: 300
+      }
+    }
+  },
+  {
+    urlPattern: /cnn/,
+    handler: 'cacheFirst',
+    options: {
+      cache:{
+      name: 'cnn-cache',
+      maxEnteries: 10,
+      maxAgeSeconds: 300
+    }
+    }
+  }]
 };
